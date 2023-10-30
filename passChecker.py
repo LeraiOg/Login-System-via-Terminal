d5203e0365e8e84d4b570ed9c2beb70560f45977
LeraiOg <86412800+LeraiOg@users.noreply.github.com>
2023-10-30 10:55:53 +0000

    going to add some more advanced stuff later including the ability to create a new account

--- passChecker.py
+++ passChecker.py
@@ -3,7 +3,9 @@
 #A Simple Password Validator
 import time
 import credentials
-print("Enter your Username")
+print("If you have an account already press 1")
+if int(input()) == 1:
+ print("Enter your username")
 if input() == credentials.user():
  print("Enter your password to gain access to Caution Club Administrator Panel")
 if input() == credentials.passP():
